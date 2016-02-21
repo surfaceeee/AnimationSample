6bdba4aafcaebe8090f630069ef9e9185a076cdb
mc110 <yuzhe@mucang.cn>
2016-02-21 23:06:04 +0800

    修复文件

---
+++ AnimationPosition/Build/Intermediates/AnimationPosition.build/Debug-iphonesimulator/AnimationPosition.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/mc110/百度云同步盘/Mooc/Animation/AnimationPosition/AnimationPosition/main.m \
+  /Users/mc110/百度云同步盘/Mooc/Animation/AnimationPosition/AnimationPosition/AppDelegate.h
